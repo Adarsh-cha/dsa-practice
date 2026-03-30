496fd91819b19fb5fe38ef58689e3aac51f42e98
Adarsh <choudharyadarsh183@gmail.com>
2026-03-31 02:21:41 +0530

    Day10

---
+++ day10/CheckifStringsCanbeMadeEqualWithOperationsII.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        int n = s1.length();
+
+        int[] even = new int[26];
+        int[] odd = new int[26];
+
+        for (int i = 0; i < n; i += 2) {
+            even[s1.charAt(i) - 'a']++;
+            even[s2.charAt(i) - 'a']--;
+        }
+
+        for (int i = 1; i < n; i += 2) {
+            odd[s1.charAt(i) - 'a']++;
+            odd[s2.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (even[i] != 0 || odd[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
