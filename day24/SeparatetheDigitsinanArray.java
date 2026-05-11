581a213790c7ddae67018b5a0d5a4d24dc2c274a
Adarsh <choudharyadarsh183@gmail.com>
2026-05-11 07:25:41 +0530

    Day24

---
+++ day24/SeparatetheDigitsinanArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int tem = nums[n - 1 - i];
+            
+            while (tem > 0) {
+                arr.add(tem % 10);
+                tem /= 10;
+            }
+        }
+
+        int[] ans = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            ans[i] = arr.get(arr.size() - 1 - i);
+        }
+        return ans;
+    }
+}
