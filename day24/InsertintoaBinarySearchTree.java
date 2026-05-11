8a0333e7442330795699db461532ead959cb854e
Adarsh <choudharyadarsh183@gmail.com>
2026-05-11 12:01:11 +0530

    Day24

---
+++ day24/InsertintoaBinarySearchTree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (curr.val > val) {
+                if (curr.left == null) {
+                    TreeNode node = new TreeNode(val);
+                    curr.left = node;
+                    break;
+                }
+                curr = curr.left;
+            }
+            else {
+                if (curr.right == null) {
+                    TreeNode node = new TreeNode(val);
+                    curr.right = node;
+                    break;
+                }
+                curr = curr.right;
+            }
+        }
+        return root;
+    }
+}
