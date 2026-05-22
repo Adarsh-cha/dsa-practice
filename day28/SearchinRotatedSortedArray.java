b4eb92e3d12c4031303ae1f58bf06a913ad2cf68
Adarsh <choudharyadarsh183@gmail.com>
2026-05-22 12:39:24 +0530

    Day 28

---
+++ day28/SearchinRotatedSortedArray.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[0] <= nums[mid]){
+                if (nums[0] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
